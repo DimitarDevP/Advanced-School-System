b0be181ae0b6481383ab9ea6c386c9aa49e52f4d
DimitarVel <dimitar.veljanovski@tradiebot.com>
2020-04-28 17:46:37 +0200

    Revert "Got rid of unneccessary files and fixed dead user commits"
    This reverts commit 4251c0a648e7a076e7d5769cd12f886a4d2b7d50.

--- backend/server/connect.py
+++
@@ -1,8 +0,0 @@
-from flaskSQL import *
-
-fsql = flaskSQL(Flask(__name__, static_url_path="/"))
-fsql.connect('root', '', '127.0.0.1', 'Advanced_School_System')
-fsql.config_cors()
-fsql.config_session()
-fsql.config_mail('smtp.sendgrid.net', 587, True, False, "apikey", "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")
-app = fsql.get_app()
